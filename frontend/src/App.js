ec6c065ae4384ae52cfb610b46bac64e45abb924
Harshaka Herath <herathharshaka@gmail.com>
2025-04-30 14:43:06 +0530

    App.js
    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "./Components/Auth/Login";
+import Register from "./Components/Auth/Register";
+import AddDonation from "./Components/Donor/AddDonation";
+import DonationDetails from "./Components/Donor/DonationDetails";
+import UpdateDonation from "./Components/Donor/UpdateDonation";
+import ClaimDonation from "./Components/NGO/ClaimDonation";
+import NgoClaims from "./Components/NGO/NgoClaims";
+import DeliveryDetails from "./Components/Delivery/DeliveryDetails";
+import UpdateDelivery from "./Components/Delivery/UpdateDelivery";
+import AdminDashboard from "./Components/Admin/AdminDashboard";
+import AdminDonations from "./Components/Admin/AdminDonations";
+import AdminDeliveries from "./Components/Admin/AdminDeliveries";
+import AssignDelivery from "./Components/Admin/AssignDelivery";
+import AdminReports from "./Components/Admin/AdminReports";
+import Home from "./Components/Home/Home";
+import Profile from "./Components/Profile/Profile";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-    <h1>Hello</h1>
-    </div>
-  );
+    return (
+        <div>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/donor" element={<DonationDetails />} />
+                <Route path="/donor/add" element={<AddDonation />} />
+                <Route path="/donor/update/:id" element={<UpdateDonation />} />
+                <Route path="/ngo" element={<NgoClaims />} />
+                <Route path="/ngo/claim" element={<ClaimDonation />} />
+                <Route path="/delivery" element={<DeliveryDetails />} />
+                <Route path="/delivery/update/:id" element={<UpdateDelivery />} />
+                <Route path="/admin" element={<AdminDashboard />} />
+                <Route path="/admin/donations" element={<AdminDonations />} />
+                <Route path="/admin/deliveries" element={<AdminDeliveries />} />
+                <Route path="/admin/assign-delivery" element={<AssignDelivery />} />
+                <Route path="/admin/reports" element={<AdminReports />} />
+                <Route path="/profile" element={<Profile />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
