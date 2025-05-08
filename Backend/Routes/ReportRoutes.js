5dcd410a7f157d82869eecd8d37ed66613428aa6
Harshaka Herath <herathharshaka@gmail.com>
2025-05-08 21:47:25 +0530

    ReportRoute.js
    Update ReportRoute.js

--- Backend/Routes/ReportRoutes.js
+++ Backend/Routes/ReportRoutes.js
@@ -4,9 +4,8 @@ const ReportController = require("../Controllers/ReportController");
 
 // Report routes
 router.get("/donations", ReportController.generateDonationReport);
-//delivery route
+router.get("/claims", ReportController.generateClaimsReport);
 router.get("/deliveries", ReportController.generateDeliveryReport);
-//user route
 router.get("/users", ReportController.generateUserReport);
 
-module.exports = router;
+module.exports = router; 
